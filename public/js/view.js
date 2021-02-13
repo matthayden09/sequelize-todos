cf262c205fdd02ea98c83543725071e4bac0689f
Matthew Hayden <matthayden09@gmail.com>
2021-02-13 12:51:27 -0600

    get and render functions done

--- public/js/view.js
+++ public/js/view.js
@@ -1,5 +1,46 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-  console.log('DOM loaded!');
 
-  // your code here...
+
+  const form = document.getElementById('todo-form')
+  const newTodoInput = document.querySelector('input.new-item')
+  const todoListSpan = document.querySelector('.todo-container')
+
+  const getTodos = () => {
+    fetch('/api/todos')
+      .then(response => response.json())
+      .then(todos => renderTodoList(todos))
+  }
+
+  const renderTodoList = todos => {
+    const todosHTML = todos.map(todo => {
+      return `<li class="list-group-item todo-item">
+      <span>test</span>
+      <input type="text" class="edit" style="display: none;">
+      <button data-id="${todo.id}" class="delete btn btn-danger">x</button>
+      <button data-id="${todo.id}" class="complete btn btn-primary">✓</button>
+    </li>`
+
+  })
+  console.log(todosHTML)
+  todoListSpan.innerHTML = todosHTML
+  }
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const text = newTodoInput.value
+
+    fetch('api/todos', {
+      method: 'POST',
+      body: JSON.stringify({ text }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(json => console.log(json))
+      .catch(err => console.error(err))
+  })
+
+  getTodos()
+
+
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
 
-// router.get()
+router.get('/api/todos', async (req, res) => {
+    try {
+        const todos = await db.Todo.findAll()
+        res.json(todos)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
 
-// router.post()
+router.post('/api/todos', async (req, res) => {
+
+    try {
+        await db.Todo.create(req.body)
+        res.status(200).send()
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+
+})
 
 // router.put()
 
